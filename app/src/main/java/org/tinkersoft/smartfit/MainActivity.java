21ff3593f25bc5511cf2474492856d22cfac21bf
K.T. Tafari <takodatafari@gmail.com>
2016-12-07 19:27:48 -0600

    init

---
+++ app/src/main/java/org/tinkersoft/smartfit/MainActivity.java
@@ -0,0 +1,25 @@
+package org.tinkersoft.smartfit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, NavigationDrawerActivity.class);
+        startActivity(intent);
+    }
+}
